9926efdea6d9800c62ebf7f9bfe59eba4adeaf4a
Ilya Voronin <ivoronin@gmail.com>
2018-02-23 13:09:12 +0000

    Prettify example document

--- example/gen-example.sh
+++ example/gen-example.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 A4_WIDTH_INCHES="8.27"
 A4_HEIGHT_INCHES="11.7"
-DPI=500
+DPI=300
 
 A4_WIDTH_PIXELS=$(echo "${A4_WIDTH_INCHES}*${DPI}" | bc)
 A4_HEIGHT_PIXELS=$(echo "${A4_HEIGHT_INCHES}*${DPI}" | bc)
 
 node ../index.js example.md example.pdf
-convert -density "${DPI}" -extent "${A4_WIDTH_PIXELS}x${A4_HEIGHT_PIXELS}" example.pdf example.png
+convert -density "${DPI}" example.pdf -extent "${A4_WIDTH_PIXELS}x${A4_HEIGHT_PIXELS}"\
+    \( +clone -background black -shadow 75x10+0+0 \)\
+    +swap  -background none -layers merge +repage\
+    example.png
