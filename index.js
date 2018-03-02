988b0d7da0dee24b936c69a938f6c3a1f231232c
Ilya Voronin <ivoronin@gmail.com>
2018-03-02 21:26:31 +0000

    Hyphenation support

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const argv = require('yargs-parser')(process.argv.slice(2), {
         template: path.join(__dirname, 'default.ejs'),
         css: fileUrl(path.join(__dirname, 'default.css')),
         renderer: 'chrome',
+        language: 'en',
     },
     configuration: {
         'parse-numbers': false,
@@ -32,7 +33,7 @@ function exit(err) {
 }
 
 if (argv._.length != 2 || !Object.keys(renderers).includes(argv.renderer)) {
-    console.log('mkd2pdf [--template template.ejs] [--css url] [--renderer <chrome|prince>] <input.md> <output.pdf>')
+    console.log('mkd2pdf [--template template.ejs] [--css url] [--renderer <chrome|prince>] [--language lang] <input.md> <output.pdf>')
     process.exit(1)
 }
 
@@ -42,7 +43,7 @@ var [input, output] = argv._
 fs.readFile(input, 'utf-8', (err, mdown) => {
     if (err) exit(err)
     content = converter.makeHtml(mdown)
-    ejs.renderFile(argv.template, { content: content, css: argv.css }, (err, html) => {
+    ejs.renderFile(argv.template, { content: content, css: argv.css, language: argv.language }, (err, html) => {
         if (err) exit(err)
         tmp.file({ postfix: '.html' }, (err, pathname, fd) => {
             if (err) exit(err)
