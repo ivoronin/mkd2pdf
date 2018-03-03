940498fa31453558c39aca731fed9ff27ace006c
Ilya Voronin <ivoronin@gmail.com>
2018-03-03 23:53:14 +0000

    Use WeasyPrint

--- example/gen-example.sh
+++ example/gen-example.sh
@@ -6,7 +6,7 @@ DPI=300
 A4_WIDTH_PIXELS=$(echo "${A4_WIDTH_INCHES}*${DPI}" | bc)
 A4_HEIGHT_PIXELS=$(echo "${A4_HEIGHT_INCHES}*${DPI}" | bc)
 
-node ../index.js example.md example.pdf
+node ../index.js -r weasyprint example.md example.pdf
 convert -density "${DPI}" example.pdf -extent "${A4_WIDTH_PIXELS}x${A4_HEIGHT_PIXELS}"\
     \( +clone -background black -shadow 75x10+0+0 \)\
     +swap  -background none -layers merge +repage\
